137c45e20e91366d69c0c0b47f9a0bac649d0828
LAPTOP-LBQ37885\Sabrina <jean66536028@gmail.com>
2025-02-11 22:21:23 +0800

    chore: build 設定、server 設定、設定路徑、啟用 Source Map

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/viewsonic-fe/",
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+  sourmap: true,
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
